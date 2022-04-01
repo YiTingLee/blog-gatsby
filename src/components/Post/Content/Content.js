57474bb41c789728be840bf97b12e7861360c4cc
Eric Lee <ranye0719@gmail.com>
2022-04-02 00:03:27 +0800

    feat: add reading time

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -1,16 +1,21 @@
 // @flow strict
-import React from 'react';
-import styles from './Content.module.scss';
+import React from "react";
+import styles from "./Content.module.scss";
 
 type Props = {
   body: string,
-  title: string
+  title: string,
+  readingTime: String,
 };
 
-const Content = ({ body, title }: Props) => (
-  <div className={styles['content']}>
-    <h1 className={styles['content__title']}>{title}</h1>
-    <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
+const Content = ({ body, title, readingTime }: Props) => (
+  <div className={styles["content"]}>
+    <h1 className={styles["content__title"]}>{title}</h1>
+    <div className={styles["content__readingTime"]}>{readingTime}</div>
+    <div
+      className={styles["content__body"]}
+      dangerouslySetInnerHTML={{ __html: body }}
+    />
   </div>
 );
 
--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,26 +1,36 @@
 // @flow strict
-import React from 'react';
-import { graphql } from 'gatsby';
-import Layout from '../components/Layout';
-import Post from '../components/Post';
-import { useSiteMetadata } from '../hooks';
-import type { MarkdownRemark } from '../types';
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Post from "../components/Post";
+import { useSiteMetadata } from "../hooks";
+import type { MarkdownRemark } from "../types";
 
 type Props = {
   data: {
-    markdownRemark: MarkdownRemark
-  }
+    markdownRemark: MarkdownRemark,
+  },
 };
 
 const PostTemplate = ({ data }: Props) => {
   const { title: siteTitle, subtitle: siteSubtitle } = useSiteMetadata();
   const { frontmatter } = data.markdownRemark;
-  const { title: postTitle, description: postDescription, socialImage } = frontmatter;
-  const metaDescription = postDescription !== null ? postDescription : siteSubtitle;
-  const socialImageUrl = typeof socialImage !== 'undefined' ? socialImage['publicURL'] : undefined;
+  const {
+    title: postTitle,
+    description: postDescription,
+    socialImage,
+  } = frontmatter;
+  const metaDescription =
+    postDescription !== null ? postDescription : siteSubtitle;
+  const socialImageUrl =
+    typeof socialImage !== "undefined" ? socialImage["publicURL"] : undefined;
 
   return (
-    <Layout title={`${postTitle} - ${siteTitle}`} description={metaDescription} socialImage={socialImageUrl} >
+    <Layout
+      title={`${postTitle} - ${siteTitle}`}
+      description={metaDescription}
+      socialImage={socialImageUrl}
+    >
       <Post post={data.markdownRemark} />
     </Layout>
   );
@@ -34,6 +44,9 @@ export const query = graphql`
       fields {
         slug
         tagSlugs
+        readingTime {
+          text
+        }
       }
       frontmatter {
         date
